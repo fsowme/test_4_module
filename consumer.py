6da0b367ee93b6e3c834e2a49d17169356ebbc0f
Vitaly Mikhailov <vmikhailov@reksoft.ru>
2025-02-23 23:40:29 +0300

    test 2

---
+++ consumer.py
@@ -0,0 +1,37 @@
+from confluent_kafka import Consumer
+
+if __name__ == "__main__":
+    consumer_conf = {
+        "bootstrap.servers": "localhost:9080",
+        "group.id": "consumer-ssl-group",
+        "auto.offset.reset": "earliest",
+
+        "security.protocol": "SASL_PLAINTEXT",
+        # "security.protocol": "SSL",
+        "ssl.ca.location": "ca.crt",  # Сертификат центра сертификации
+        "ssl.certificate.location": "kafka-1-creds/kafka-1.crt",  # Сертификат клиента Kafka
+        "ssl.key.location": "kafka-1-creds/kafka-1.key",  # Приватный ключ для клиента Kafka
+
+
+        "sasl.mechanism": "PLAIN",
+        "sasl.username": "admin",  # Имя пользователя для аутентификации
+        "sasl.password": "admin-secret",  # Пароль пользователя для аутентификации
+    }
+    consumer = Consumer(consumer_conf)
+    consumer.subscribe(["sasl-plain-topic"])
+
+    try:
+        while True:
+            message = consumer.poll(0.1)
+
+            if message is None:
+                continue
+            if message.error():
+                print(f"Ошибка: {message.error()}")
+                continue
+
+            key = message.key().decode("utf-8")
+            value = message.value().decode("utf-8")
+            print(f"Получено сообщение: {key=}, {value=}, offset={message.offset()}")
+    finally:
+        consumer.close()
---
+++ producer.py
@@ -0,0 +1,33 @@
+import uuid
+
+from confluent_kafka import Producer
+import time
+
+if __name__ == "__main__":
+    producer_conf = {
+        "bootstrap.servers": "localhost:9080",
+
+        # Настройки безопасности SSL
+        "security.protocol": "SASL_PLAINTEXT",
+        "ssl.ca.location": "ca.crt",  # Сертификат центра сертификации
+        "ssl.certificate.location": "kafka-1-creds/kafka-1.crt",  # Сертификат клиента Kafka
+        "ssl.key.location": "kafka-1-creds/kafka-1.key",  # Приватный ключ для клиента Kafka
+
+        # Настройки SASL-аутентификации
+        "sasl.mechanism": "PLAIN",  # Используемый механизм SASL (PLAIN)
+        "sasl.username": "admin",  # Имя пользователя для аутентификации
+        "sasl.password": "admin-secret",  # Пароль пользователя для аутентификации
+    }
+
+    producer = Producer(producer_conf)
+    while True:
+        key = f"key-{uuid.uuid4()}"
+        value = "SASL/PLAIN"
+        producer.produce(
+            "sasl-plain-topic",
+            key=key,
+            value=value,
+        )
+        producer.flush()
+        print(f"Отправлено сообщение: {key=}, {value=}")
+        time.sleep(1)
